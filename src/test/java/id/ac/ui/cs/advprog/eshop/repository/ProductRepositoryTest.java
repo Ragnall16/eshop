c64f0dc9cb98b8f3ecca4b43e3dca81b020cefc4
Ragnall16 <ragnall.malfath@gmail.com>
2025-02-13 08:30:26 +0700

    Created unit test for ProductRepository class

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -0,0 +1,66 @@
+package id.ac.ui.cs.advprog.eshop.repository;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Iterator;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ProductRepositoryTest {
+
+    @InjectMocks
+    ProductRepository productRepository;
+    @BeforeEach
+    void setUp() {
+    }
+    @Test
+    void testCreateAndFind() {
+        Product product = new Product();
+        product.setProductId("573e7b70-589f-49c2-8385-319533c676be");
+        product.setProductName("Manchester United Home Jersey 24/25 ");
+        product.setProductQuantity(7);
+        productRepository.create(product);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product savedProduct = productIterator.next();
+        assertEquals(product.getProductId(), savedProduct.getProductId());
+        assertEquals(product.getProductName(), savedProduct.getProductName());
+        assertEquals(product.getProductQuantity(), savedProduct.getProductQuantity());
+    }
+
+    @Test
+    void testFindAllfEmpty() {
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertFalse(productIterator.hasNext());
+    }
+    @Test
+    void testFindAllIfMoreThanOneProduct() {
+        Product product1 = new Product();
+        product1.setProductId("573e7b70-589f-49c2-8385-319533c676be");
+        product1.setProductName("Manchester United Home Jersey 24/25 ");
+        product1.setProductQuantity(7);
+        productRepository.create(product1);
+
+        Product product2 = new Product();
+        product2.setProductId("87c1e40b-b403-4af5-b12d-8de6dfb699bc");
+        product2.setProductName("Manchester United Away Jersey 23/24 ");
+        product2.setProductQuantity(8);
+        productRepository.create(product2);
+
+        Iterator<Product> productIterator = productRepository.findAll();
+        assertTrue(productIterator.hasNext());
+        Product savedProduct = productIterator.next();
+        assertEquals(product1.getProductId(), savedProduct.getProductId());
+        savedProduct = productIterator.next();
+        assertEquals(product2.getProductId(), savedProduct.getProductId());
+        assertFalse(productIterator.hasNext());
+    }
+}
