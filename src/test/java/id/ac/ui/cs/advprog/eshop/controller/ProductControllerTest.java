68b09ae35f4bd7243bcf9a7c168bb12c5d7d42dc
Ragnall16 <ragnall.malfath@gmail.com>
2025-02-18 20:37:59 +0700

    Fix case sensitive typo in 2 tests on ProductControllerTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -49,7 +49,7 @@ class ProductControllerTest {
     void testCreateProductPage() throws Exception {
         mockMvc.perform(get("/product/create"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("createProduct"))
+                .andExpect(view().name("CreateProduct"))
                 .andExpect(model().attributeExists("product"));
     }
 
@@ -71,7 +71,7 @@ class ProductControllerTest {
 
         mockMvc.perform(get("/product/list"))
                 .andExpect(status().isOk())
-                .andExpect(view().name("productList"))
+                .andExpect(view().name("ProductList"))
                 .andExpect(model().attribute("products", hasSize(2)))
                 .andExpect(model().attribute("products", contains(product1, product2)));
     }
