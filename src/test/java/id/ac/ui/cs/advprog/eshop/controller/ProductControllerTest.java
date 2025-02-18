7365f175498d319f480b85c97213be7de9d4cae6
Ragnall16 <ragnall.malfath@gmail.com>
2025-02-18 20:04:00 +0700

    Created a unit test for ProductController (Improved to code coverage to 100% !!!)

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -0,0 +1,109 @@
+package id.ac.ui.cs.advprog.eshop.controller;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+import id.ac.ui.cs.advprog.eshop.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(ProductController.class)
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ProductService productService;
+
+    private Product product1;
+    private Product product2;
+
+    @BeforeEach
+    void setUp() {
+        // Use actual UUIDs for productId
+        product1 = new Product();
+        product1.setProductId(UUID.randomUUID().toString());
+        product1.setProductName("French Fries");
+        product1.setProductQuantity(100);
+
+        product2 = new Product();
+        product2.setProductId(UUID.randomUUID().toString());
+        product2.setProductName("Potato Wedges");
+        product2.setProductQuantity(200);
+    }
+
+    @Test
+    void testCreateProductPage() throws Exception {
+        mockMvc.perform(get("/product/create"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("createProduct"))
+                .andExpect(model().attributeExists("product"));
+    }
+
+    @Test
+    void testCreateProductPost() throws Exception {
+        mockMvc.perform(post("/product/create")
+                        .param("productName", "French Fries")
+                        .param("productQuantity", "100"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(productService, times(1)).create(any(Product.class));
+    }
+
+    @Test
+    void testProductListPage() throws Exception {
+        List<Product> products = Arrays.asList(product1, product2);
+        when(productService.findAll()).thenReturn(products);
+
+        mockMvc.perform(get("/product/list"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("productList"))
+                .andExpect(model().attribute("products", hasSize(2)))
+                .andExpect(model().attribute("products", contains(product1, product2)));
+    }
+
+    @Test
+    void testEditProductPage() throws Exception {
+        when(productService.findAll()).thenReturn(Arrays.asList(product1, product2));
+
+        mockMvc.perform(get("/product/edit/" + product1.getProductId()))
+                .andExpect(status().isOk())
+                .andExpect(view().name("EditProduct"))
+                .andExpect(model().attribute("product", product1));
+    }
+
+    @Test
+    void testEditProductPost() throws Exception {
+        mockMvc.perform(post("/product/edit")
+                        .param("productId", product1.getProductId())
+                        .param("productName", "Mashed Potatoes")
+                        .param("productQuantity", "150"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("list"));
+
+        verify(productService, times(1)).update(any(Product.class));
+    }
+
+    @Test
+    void testDeleteProduct() throws Exception {
+        mockMvc.perform(get("/product/delete/" + product1.getProductId()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/product/list"));
+
+        verify(productService, times(1)).delete(product1.getProductId());
+    }
+}
