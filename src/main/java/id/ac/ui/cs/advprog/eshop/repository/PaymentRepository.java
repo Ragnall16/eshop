b681a735e9bc661cc176949c791ed01c078678aa
Ragnall16 <ragnall.malfath@gmail.com>
2025-03-04 11:35:00 +0700

    [REFACTOR] Apply PaymentStatus enum check to PaymentRepository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -3,6 +3,8 @@ package id.ac.ui.cs.advprog.eshop.repository;
 import id.ac.ui.cs.advprog.eshop.model.Payment;
 import id.ac.ui.cs.advprog.eshop.model.Order;
 import org.springframework.stereotype.Repository;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
+import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,10 +20,10 @@ public class PaymentRepository {
     public Payment save(Order order, Payment payment) {
         paymentData.add(payment);
         paymentOrder.put(payment.getId(), order);
-        if (payment.getStatus().equals("SUCCESS")) {
-            order.setStatus("SUCCESS");
-        } else if (payment.getStatus().equals("REJECTED")) {
-            order.setStatus("FAILED");
+        if (payment.getStatus().equals(PaymentStatus.SUCCESS.getValue())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (payment.getStatus().equals(PaymentStatus.REJECTED.getValue())) {
+            order.setStatus(OrderStatus.FAILED.getValue());
         } else {
             throw new IllegalArgumentException();
         }
@@ -32,10 +34,10 @@ public class PaymentRepository {
     public void update(Payment payment, String status) {
         Order order = this.getOrder(payment.getId());
         payment.setStatus(status);
-        if (status.equals("SUCCESS")) {
-            order.setStatus("SUCCESS");
-        } else if (status.equals("REJECTED")) {
-            order.setStatus("FAILED");
+        if (status.equals(PaymentStatus.SUCCESS.getValue())) {
+            order.setStatus(OrderStatus.SUCCESS.getValue());
+        } else if (status.equals(PaymentStatus.REJECTED.getValue())) {
+            order.setStatus(OrderStatus.FAILED.getValue());
         } else {
             throw new IllegalArgumentException();
         }
